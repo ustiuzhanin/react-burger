a46f55bc0fa47da2bc8d9daae7d433b6a91ee27d
Slava <ucheba00@gmail.com>
2019-03-03 22:31:00 -0700

    Updates

---
+++ precache-manifest.46fa6267ff08922e60dd4ee11193cfa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "609cd8a25712f9517921",
+    "url": "/react-burger/static/css/main.ca87a1ba.chunk.css"
+  },
+  {
+    "revision": "609cd8a25712f9517921",
+    "url": "/react-burger/static/js/main.609cd8a2.chunk.js"
+  },
+  {
+    "revision": "eb1b0e8ef7ed527f6b1a",
+    "url": "/react-burger/static/js/runtime~main.eb1b0e8e.js"
+  },
+  {
+    "revision": "42cee9392a81b9e78132",
+    "url": "/react-burger/static/js/2.42cee939.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "12d83ae66ff5f0a14455082800c6eb53",
+    "url": "/react-burger/index.html"
+  }
+];
